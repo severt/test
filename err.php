54c5630088cb9ffc2575ec8305713326bfadddd9
Sivakov Sergey <severt@gmail.com>
2025-12-17 16:48:37 +0300

    Update err.php

--- err.php
+++ err.php
@@ -1,5 +1,12 @@
- nodeJS - pubsub message start:{"type":7,"ctx":{"logger":{"category":"nodeJS","context":{"TENANT":"
-localhost","DOCID":"ff4155688361c542f8a8d728912d193e","USERID":"41403"},"callStackSkipIndex":0},"tenant":"localhost","docId":"ff4155688361c542f8a8d728912d193e","userId":"41403"},"cmd":{"w
-ithAuthorization":true,"c":"open","id":"ff4155688361c542f8a8d728912d193e","userid":"41403","format":"docx","url":"https://kp-iod-app-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.p
-hp?type=download&ID=383","title":"777777777.docx","outputformat":8192,"outputpath":"Editor.bin","embeddedfonts":false,"status_info":-81,"lcid":25,"nobase64":true,"convertToOrigin":".pdf.x
-ps.oxps.djvu"},"output":{"type":"documentOpen","data":{"type":"open","status":"err","data":-81}},"needUrlKey":null,"needUrlMethod":null,"needUrlType":null}
+[2025-12-17T16:45:51.617] [ERROR] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - error downloadFile:url=https://kp-iod-app-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=download&ID=383;attempt=3;co
+    at TLSSocket.onConnectSecure (_tls_wrap.js:1515:34)
+    at TLSSocket.emit (events.js:400:28)
+    at TLSSocket._finishInit (_tls_wrap.js:937:8)
+    at TLSWrap.ssl.onhandshakedone (_tls_wrap.js:709:12)
+[2025-12-17T16:45:52.618] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - ExitCode (code=0;signal=null;error:-81)
+[2025-12-17T16:45:52.619] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - output (data={"ctx":{"logger":{"category":"nodeJS","context":{"TENANT":"localhost","DOCID":"ff4155688361c542f8a8d728912d193e","USERI
+[2025-12-17T16:45:52.619] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - postProcess
+[2025-12-17T16:45:52.620] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - deleteFolderRecursive
+[2025-12-17T16:45:52.620] [INFO] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - End Task
+[2025-12-17T16:45:52.623] [INFO] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - ackTask addResponse
+[2025-12-17T16:45:52.623] [INFO] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - ackTask ack
