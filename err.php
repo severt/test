0f105d965713f162edaa8d81fbd9e1b401ccedce
Sivakov Sergey <severt@gmail.com>
2025-12-18 14:39:32 +0300

    Update err.php

--- err.php
+++ err.php
@@ -1,12 +1,6 @@
-[2025-12-17T16:45:51.617] [ERROR] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - error downloadFile:url=https://kp-iod-app-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=download&ID=383;attempt=3;co
+[2025-12-18T14:33:02.449] [ERROR] [localhost] [b3e344c5f50d604db759dfd4939e86d1] [5518] nodeJS - error downloadFile:url=https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=download&ID=58
+90434;attempt=3;code:SELF_SIGNED_CERT_IN_CHAIN;connect:undefined Error: self signed certificate in certificate chain
     at TLSSocket.onConnectSecure (_tls_wrap.js:1515:34)
     at TLSSocket.emit (events.js:400:28)
     at TLSSocket._finishInit (_tls_wrap.js:937:8)
     at TLSWrap.ssl.onhandshakedone (_tls_wrap.js:709:12)
-[2025-12-17T16:45:52.618] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - ExitCode (code=0;signal=null;error:-81)
-[2025-12-17T16:45:52.619] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - output (data={"ctx":{"logger":{"category":"nodeJS","context":{"TENANT":"localhost","DOCID":"ff4155688361c542f8a8d728912d193e","USERI
-[2025-12-17T16:45:52.619] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - postProcess
-[2025-12-17T16:45:52.620] [DEBUG] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - deleteFolderRecursive
-[2025-12-17T16:45:52.620] [INFO] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - End Task
-[2025-12-17T16:45:52.623] [INFO] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - ackTask addResponse
-[2025-12-17T16:45:52.623] [INFO] [localhost] [ff4155688361c542f8a8d728912d193e] [41403] nodeJS - ackTask ack
