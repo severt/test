7dd81f4ced10c3cfc6b4c44533fcc442811c4a59
Sivakov Sergey <severt@gmail.com>
2025-12-18 15:33:13 +0300

    Update err.php

--- err.php
+++ err.php
@@ -1,6 +1,21 @@
-[2025-12-18T14:33:02.449] [ERROR] [localhost] [b3e344c5f50d604db759dfd4939e86d1] [5518] nodeJS - error downloadFile:url=https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=download&ID=58
-90434;attempt=3;code:SELF_SIGNED_CERT_IN_CHAIN;connect:undefined Error: self signed certificate in certificate chain
-    at TLSSocket.onConnectSecure (_tls_wrap.js:1515:34)
-    at TLSSocket.emit (events.js:400:28)
-    at TLSSocket._finishInit (_tls_wrap.js:937:8)
-    at TLSWrap.ssl.onhandshakedone (_tls_wrap.js:709:12)
+curl -v 'https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewwer/ajax.php?type=download&ID=58'
+*   Trying 10.56.144.150:443...
+* Connected to kp-gp-app1-prd.codm.gazprom.loc (10.56.144.150) port 443 (#0)
+* ALPN: offers h2
+* ALPN: offers http/1.1
+*  CAfile: /etc/pki/tls/certs/ca-bundle.crt
+*  CApath: none
+* TLSv1.3 (OUT), TLS handshake, Client hello (1):
+* TLSv1.3 (IN), TLS handshake, Server hello (2):
+* TLSv1.3 (IN), TLS handshake, Encrypted Extensions (8):
+* TLSv1.3 (IN), TLS handshake, Certificate (11):
+* TLSv1.3 (OUT), TLS alert, unknown CA (560):
+* SSL certificate problem: self signed certificate in certificate chain
+* Closing connection 0
+curl: (60) SSL certificate problem: self signed certificate in certificate chain
+More details here: https://curl.se/docs/sslcerts.html
+
+curl failed to verify the legitimacy of the server and therefore could not
+establish a secure connection to it. To learn more about this situation and
+how to fix it, please visit the web page mentioned above.
+[root@kp-gp-arr1-prd sites-enabled]#
