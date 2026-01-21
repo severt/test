ad8a45d8b2180dfa6acff733d880ccc06f94c6a1
Sivakov Sergey <severt@gmail.com>
2026-01-21 11:33:29 +0300

    Update err.php

--- err.php
+++ err.php
@@ -1,44 +1,73 @@
-echo -n | openssl s_client -connect kp-gp-r72-prd.codm.gazprom.loc:443 -servername kp-gp-r72-prd.codm.gazprom.loc | openssl x509 > /etc/pki/tls/certs/r7-office-ca.crt
-
-Узнайте, где лежит системный бандл: /etc/pki/tls/certs/ca-bundle.crt.
-Создайте свой комбинированный файл:
-cat /etc/pki/tls/certs/ca-bundle.crt /etc/pki/tls/certs/r7-office-ca.crt > /etc/pki/tls/certs/php-ca-bundle.crt
-
-
-Для CentOS/RHEL (как у вас):
-
-Узнайте, где лежит системный бандл: /etc/pki/tls/certs/ca-bundle.crt.
-
-Создайте свой комбинированный файл:
-
-bash
-cat /etc/pki/tls/certs/ca-bundle.crt /etc/pki/tls/certs/r7-office-ca.crt > /etc/pki/tls/certs/php-ca-bundle.crt
-(Теперь в php-ca-bundle.crt есть и все мировые CA, и ваш Р7).
-
-Для Ubuntu/Debian:
-Системный путь обычно /etc/ssl/certs/ca-certificates.crt. Логика та же:
-cat /etc/ssl/certs/ca-certificates.crt /путь/к/r7.crt > /etc/ssl/certs/php-custom.crt
-
-3. Настройка php.ini
-Найдите активный php.ini (в консоли php --ini или php -i | grep 'Loaded Configuration File'). Часто для веб-сервера (PHP-FPM) он лежит отдельно, например /etc/php/7.4/fpm/php.ini.
-
-Откройте его на редактирование.
-
-Найдите директиву openssl.cafile.
-
-Раскомментируйте и укажите путь к вашему новому файлу:
-
-text
-openssl.cafile = /etc/pki/tls/certs/php-ca-bundle.crt
-Если используете cURL напрямую, можно также задать curl.cainfo:
-
-text
-curl.cainfo = /etc/pki/tls/certs/php-ca-bundle.crt
-4. Перезапуск PHP
-Чтобы изменения вступили в силу, перезапустите службу PHP-FPM (или Apache):
-
-bash
-# Для CentOS/RHEL
-systemctl restart php-fpm
-# или
-systemctl restart httpd
+[2026-01-21T11:08:45.720] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55181] nodeJS - clientLog: onDocumentContentReady time:1222 memory:{"totalJSHeapSize":164681747,"usedJSHeapSize":152081079,"jsHeapSizeLimit":4294705152}
+[2026-01-21T11:08:47.847] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55181] nodeJS - Connection closed or timed out: reason = transport close
+[2026-01-21T11:09:20.886] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - io.use start
+[2026-01-21T11:09:20.886] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - io.use end
+[2026-01-21T11:09:20.887] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - _checkLicense start
+[2026-01-21T11:09:20.887] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - sendData: type = license
+[2026-01-21T11:09:20.887] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - _checkLicense end
+[2026-01-21T11:09:20.942] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - data.type = auth
+[2026-01-21T11:09:20.942] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - auth time: 1468
+[2026-01-21T11:09:20.961] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - sendData: type = auth
+[2026-01-21T11:09:20.962] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - Start command: {"withAuthorization":true,"c":"open","id":"63d33e0823c8d340b3399266d94bb95c","userid":"5518","format":"docx","url":"https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=download&ID=5985329","title":"111.docx","lcid":9,"nobase64":true,"convertToOrigin":".pdf.xps.oxps.djvu"}
+[2026-01-21T11:09:20.964] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - Response command: {"type":"open","status":"ok","data":{"Editor.bin":"https://kp-gp-r72-prd.adm.gazprom.ru/cache/files/data/63d33e0823c8d340b3399266d94bb95c/Editor.bin/Editor.bin?md5=Q0jR3Vp-IPHfpNeGmLDpZQ&expires=1771598685&filename=Editor.bin"},"openedAt":1768993724910}
+[2026-01-21T11:09:20.964] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - sendData: type = documentOpen
+[2026-01-21T11:09:20.964] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - End command
+[2026-01-21T11:09:24.024] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - data.type = clientLog
+[2026-01-21T11:09:24.024] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - clientLog: onDownloadFile time:3027
+[2026-01-21T11:09:24.727] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - data.type = clientLog
+[2026-01-21T11:09:24.727] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - clientLog: onOpenDocument time:172
+[2026-01-21T11:09:25.055] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - data.type = clientLog
+[2026-01-21T11:09:25.055] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - clientLog: onLoadFonts time:331
+[2026-01-21T11:09:25.474] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - data.type = cursor
+[2026-01-21T11:09:25.474] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - data.type = clientLog
+[2026-01-21T11:09:25.475] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - send cursor: {
+  cursor: '',
+  time: 1768982965475,
+  user: '55182',
+  useridoriginal: '5518'
+}
+[2026-01-21T11:09:25.475] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - clientLog: onDocumentContentReady time:5897 memory:{"totalJSHeapSize":165388814,"usedJSHeapSize":150711886,"jsHeapSizeLimit":4294705152}
+[2026-01-21T11:09:31.650] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55182] nodeJS - Connection closed or timed out: reason = transport close
+[2026-01-21T11:09:32.207] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - io.use start
+[2026-01-21T11:09:32.207] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - io.use end
+[2026-01-21T11:09:32.208] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - _checkLicense start
+[2026-01-21T11:09:32.208] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - sendData: type = license
+[2026-01-21T11:09:32.208] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - _checkLicense end
+[2026-01-21T11:09:32.422] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - data.type = auth
+[2026-01-21T11:09:32.422] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [userId] nodeJS - auth time: 836
+[2026-01-21T11:09:32.426] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - getCallbackByUserIndex: userIndex = 3 callbacks = {"userIndex":3,"callback":"https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=track&ID=5985329&user_id=5518"}
+[2026-01-21T11:09:32.427] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - postData request: url = https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=track&ID=5985329&user_id=5518;data = {"key":"63d33e0823c8d340b3399266d94bb95c","status":1,"users":["5518"],"actions":[{"type":1,"userid":"5518"}]}
+[2026-01-21T11:09:32.529] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - postData response: data = {"error":0,"status":"success"}
+[2026-01-21T11:09:32.538] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - sendData: type = auth
+[2026-01-21T11:09:32.538] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - Start command: {"withAuthorization":true,"wopiParams":null,"c":"open","id":"63d33e0823c8d340b3399266d94bb95c","userid":"5518","format":"docx","url":"https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=download&ID=5985329","title":"111.docx","lcid":9,"nobase64":true,"convertToOrigin":".pdf.xps.oxps.djvu"}
+[2026-01-21T11:09:32.540] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - Response command: {"type":"open","status":"ok","data":{"Editor.bin":"https://kp-gp-r72-prd.adm.gazprom.ru/cache/files/data/63d33e0823c8d340b3399266d94bb95c/Editor.bin/Editor.bin?md5=Q0jR3Vp-IPHfpNeGmLDpZQ&expires=1771598685&filename=Editor.bin"},"openedAt":1768993724910}
+[2026-01-21T11:09:32.540] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - sendData: type = documentOpen
+[2026-01-21T11:09:32.540] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [5518] nodeJS - End command
+[2026-01-21T11:09:32.576] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - data.type = clientLog
+[2026-01-21T11:09:32.576] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - clientLog: onDownloadFile time:27
+[2026-01-21T11:09:32.949] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - data.type = clientLog
+[2026-01-21T11:09:32.949] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - clientLog: onOpenDocument time:203
+[2026-01-21T11:09:32.999] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - data.type = clientLog
+[2026-01-21T11:09:32.999] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - clientLog: onLoadFonts time:51
+[2026-01-21T11:09:33.362] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - data.type = clientLog
+[2026-01-21T11:09:33.362] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - clientLog: onDocumentContentReady time:1685 memory:{"totalJSHeapSize":249424131,"usedJSHeapSize":224175879,"jsHeapSizeLimit":4294705152}
+[2026-01-21T11:09:36.784] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - data.type = isSaveLock
+[2026-01-21T11:09:36.784] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - isSaveLock lockRes: true
+[2026-01-21T11:09:36.785] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - sendData: type = saveLock
+[2026-01-21T11:09:36.795] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - data.type = saveChanges
+[2026-01-21T11:09:36.795] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - Start saveChanges: reSave: undefined
+[2026-01-21T11:09:36.797] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - saveChanges: deleteIndex: -1 ; startIndex: 0 ; length: 6
+[2026-01-21T11:09:36.800] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - sendData: type = unSaveLock
+[2026-01-21T11:09:43.772] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - Connection closed or timed out: reason = transport close
+[2026-01-21T11:09:48.779] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - Start saveFromChanges
+[2026-01-21T11:09:48.780] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - getCallbackByUserIndex: userIndex = undefined callbacks = {"userIndex":3,"callback":"https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=track&ID=5985329&user_id=5518"}
+[2026-01-21T11:09:48.786] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - AddTask saveFromChanges
+[2026-01-21T11:09:50.098] [INFO] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - receiveTask start: {"ctx":{"logger":{"category":"nodeJS","context":{"TENANT":"localhost","DOCID":"63d33e0823c8d340b3399266d94bb95c","USERID":"55183"},"callStackSkipIndex":0},"tenant":"localhost","docId":"63d33e0823c8d340b3399266d94bb95c","userId":"55183"},"cmd":{"wopiParams":null,"c":"sfc","id":"63d33e0823c8d340b3399266d94bb95c","userid":"5518","userindex":3,"data":null,"title":"output.docx","outputformat":65,"outputpath":"output.docx","status_info":0,"savekey":"63d33e0823c8d340b3399266d94bb95c_5118","jsonparams":"{\"documentLayout\":{\"openedAt\":1768993724910}}","useractionid":"5518","useractionindex":3,"nobase64":true,"status_info_in":583777}}
+[2026-01-21T11:09:50.100] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - Start commandSfcCallback
+[2026-01-21T11:09:50.101] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - getCallbackByUserIndex: userIndex = 3 callbacks = {"userIndex":3,"callback":"https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=track&ID=5985329&user_id=5518"}
+[2026-01-21T11:09:50.101] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - Callback commandSfcCallback: callback = https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=track&ID=5985329&user_id=5518
+[2026-01-21T11:09:50.107] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - commandSfcCallback presence: count = 0
+[2026-01-21T11:09:50.109] [DEBUG] [localhost] [63d33e0823c8d340b3399266d94bb95c] [55183] nodeJS - postData request: url = https://kp-gp-app1-prd.codm.gazprom.loc/local/components/r7/viewer/ajax.php?type=track&ID=5985329&user_id=5518;data = {"key":"63d33e0823c8d340b3399266d94bb95c","status":2,"url":"https://kp-gp-r72-prd.adm.gazprom.ru/cache/files/data/63d33e0823c8d340b3399266d94bb95c_5118/output.docx/output.docx?md5=Xkoysp9LAsIyRO789cvPRQ&expires=1768983891&filename=output.docx","changesurl":"https://kp-gp-r72-prd.adm.gazprom.ru/cache/files/data/63d33e0823c8d340b3399266d94bb95c_5118/changes.zip/changes.zip?md5=ExiFw-JVX6PZLVeowCyCUA&expires=1768983891&filename=changes.zip","history":{"serverVersion":"2024.3.2","changes":[{"created":"2026-01-21 08:09:36","user":{"id":"5518","name":"Сиваков С. В."}}]},"users":["5518"],"actions":[{"type":0,"userid":"5518"}],"lastsave":"2026-01-21T08:09:36.000Z","notmodified":false,"filetype":"docx"}
+[2026-01-21T11:10:26.069] [INFO] [localhost] [docId] [userId] nodeJS - checkDocumentExpire start
+[2026-01-21T11:10:26.070] [INFO] [localhost] [docId] [userId] nodeJS - checkDocumentExpire end: startSaveCount = 0, removedCount = 0
+[2026-01-21T11:10:26.071] [DEBUG] [localhost] [docId] [userId] nodeJS - expireDoc connections.length = 3
